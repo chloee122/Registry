1b1bc5c5ab11730cc1c866d28080e3b64d1bd4e2
Chloe <nguyeney02@gmail.com>
2024-03-08 17:49:08 +0200

    WIP-3

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import HomePage from "./pages/HomePage";
 import DetailsPage from "./pages/DetailsPage";
 import SearchPage from "./pages/SearchPage";
 
+interface SearchResponse {
+  data: {
+    objects:{}[]
+  };
+}
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,6 +22,21 @@ const router = createBrowserRouter([
       {
         path: "/search",
         element: <SearchPage />,
+        loader: async ({ request }) => {
+          const { searchParams } = new URL(request.url);
+
+          const term = searchParams.get("term");
+          if (!term) {
+            throw new Error("Search term must be provided");
+          }
+
+          const res = await fetch(
+            `https://registry.npmjs.org/-/v1/search?text=${term}`
+          );
+          const data: SearchResponse = await res.json();
+          console.log(data);
+          return data.objects;
+        },
       },
       { path: "packages/:name", element: <DetailsPage /> },
     ],
