5d44cbb251a5f251feca333c4c6bf4297c654c3d
chloee122 <130465629+chloee122@users.noreply.github.com>
2024-03-08 10:42:31 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import SearchPage from "./pages/SearchPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/search",
+        element: <SearchPage />,
+      },
+      { path: "packages/:name", element: <DetailsPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
